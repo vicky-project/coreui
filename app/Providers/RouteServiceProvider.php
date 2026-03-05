776ff7d9cf56908bbb0cde66700cfaa458adcdb4
vicky <pratamavr@gmail.com>
2026-03-05 20:47:17 +0800

    updates

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    protected string $name = 'Dashboard';
+    protected string $name = 'CoreUI';
 
     /**
      * Called before routes are registered.
