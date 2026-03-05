e890c340da06670295e1521b5821e9cdce2df115
vicky <pratamavr@gmail.com>
2026-03-05 23:37:23 +0800

    updates

--- resources/views/partials/app/navbar.blade.php
+++ resources/views/partials/app/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-custom">
   <div class="container">
-    <a class="navbar-brand" href="{{ route('user.dashboard') }}">
+    <a class="navbar-brand" href="{{ route('dashboard') }}">
       <i class="bi bi-grid"></i> {{ config('app.name') }}
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarUser" aria-controls="navbarUser">
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarUser">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link {{ request()->routeIs('user.dashboard') ? 'active' : '' }}" href="{{ route('user.dashboard') }}">
+          <a class="nav-link {{ request()->routeIs('dashboard') ? 'active' : '' }}" href="{{ route('dashboard') }}">
             <i class="bi bi-speedometer2"></i> Dashboard
           </a>
         </li>
