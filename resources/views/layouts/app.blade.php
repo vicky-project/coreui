6205dafce8d83774b41bde2cbdbe2d9d9233aeef
vicky <pratamavr@gmail.com>
2026-03-14 01:49:32 +0800

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,30 +73,9 @@
   <script src="https://telegram.org/js/telegram-web-app.js?61"></script>
 
   <script>
-    // Telegram Mini App detection
-    // Inisialisasi Telegram WebApp
-    const tg = window.Telegram?.WebApp;
-    if (tg?.initData) {
-      applyTelegramTheme();
-      tg.onEvent('themeChanged', function() {
-      applyTelegramTheme();
-      });
-      tg.BackButton.isVisible = true;
-      tg.onEvent("backButtonClicked",
-      function () {
-      const urlObj = new URL('{{ config("coreui.home_url") }}', window.location.origin);
-      urlObj.searchParams.set("initData", tg.initData);
-      window.location = urlObj.toString();
-      });
-      tg.BackButton.show();
-      tg.setBottomBarColor(tg.themeParams.bottom_bar_bg_color);
-      tg.requestFullscreen();
-      tg.expand();
-      tg.ready();
-    }
-
     // Terapkan tema Telegram ke CSS variables
     function applyTelegramTheme() {
+      const tg = window.Telegram?.WebApp;
       if (tg && tg.themeParams) {
         const theme = tg.themeParams;
         document.documentElement.style.setProperty('--tg-theme-bg-color', theme.bg_color || '#ffffff');
@@ -143,6 +122,28 @@
           document.documentElement.style.setProperty('--tg-theme-header-bg-color', '#ffffff');
         }
       }
+    }
+
+    // Telegram Mini App detection
+    // Inisialisasi Telegram WebApp
+    const tg = window.Telegram?.WebApp;
+    if (tg?.initData) {
+      tg.requestFullscreen();
+      applyTelegramTheme();
+      tg.onEvent('themeChanged', function() {
+      applyTelegramTheme();
+      });
+      tg.BackButton.isVisible = true;
+      tg.onEvent("backButtonClicked",
+      function () {
+      const urlObj = new URL('{{ config("coreui.home_url") }}', window.location.origin);
+      urlObj.searchParams.set("initData", tg.initData);
+      window.location = urlObj.toString();
+      });
+      tg.BackButton.show();
+      tg.setBottomBarColor(tg.themeParams.bottom_bar_bg_color);
+      tg.expand();
+      tg.ready();
     }
 
     // Fungsi toast
