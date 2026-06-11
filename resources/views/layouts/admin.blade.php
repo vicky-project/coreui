c0631842fe74856e38d0543e44ce93c6dd87e55b
vicky <pratamavr@gmail.com>
2026-06-12 00:18:51 +0800

    updates

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -67,6 +67,10 @@
       </p>
     </div>
   </div>
+  <script src="//cdn.jsdelivr.net/npm/eruda"></script>
+  <script>
+    eruda.init();
+  </script>
 
   <!-- Bootstrap JS Bundle -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
